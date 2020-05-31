6e04dee137b481b9062de36e5e67925c79cc7948
tvio <tvio@centrum.cz>
2020-05-31 19:48:17 +0200

    prvni axios get init state

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,30 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Dny from './components/Dny.vue'
-import Header from './views/Header.vue'
-import { mapState } from 'vuex'
+import Vue from "vue";
+import Dny from "./components/Dny.vue";
+import Header from "./views/Header.vue";
+import { mapState } from "vuex";
+import { mapActions } from "vuex";
 export default Vue.extend({
-  name: 'App',
+  name: "App",
 
   components: {
     Dny,
-    Header
+    Header,
   },
   data: () => ({
-    login: false
+    login: false,
     //
   }),
+  methods: {
+    ...mapActions(["initLoad"]),
+  },
   computed: {
-    ...mapState(['idToken'])
-  }
-})
+    ...mapState(["idToken"]),
+  },
+  mounted() {
+    this.initLoad();
+  },
+});
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,124 +1,223 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     dny: [
-      { id: 9, datum: '1.1.2021', den: 'po', kdo: 'Janik', pozn1: 'pozn1', pozn2: 'pozn2', pozn3: 'pozn3' },
-      { id: 2, datum: '2.1.2021', den: 'ut', kdo: 'Janik', pozn1: 'pozn1', pozn2: 'pozn2', pozn3: 'pozn3' },
-      { id: 3, datum: '3.1.2021', den: 'st', kdo: 'Janik', pozn1: 'pozn1', pozn2: 'pozn2', pozn3: 'pozn3' },
-      { id: 4, datum: '4.1.2021', den: 'ct', kdo: 'Janik', pozn1: 'pozn1', pozn2: 'pozn2', pozn3: 'pozn3' },
-      { id: 5, datum: '5.1.2021', den: 'pa', kdo: 'Dalibor', pozn1: 'pozn1', pozn2: 'pozn2', pozn3: 'pozn3' },
-      { id: 6, datum: '6.1.2021', den: 'so', kdo: 'Dalibor', pozn1: 'pozn1', pozn2: 'pozn2', pozn3: 'pozn3' },
-      { id: 7, datum: '7.1.2021', den: 'ne', kdo: 'Dalibor', pozn1: 'pozn1', pozn2: 'pozn2', pozn3: 'pozn3' }
+      {
+        id: 9,
+        datum: "1.1.2021",
+        den: "po",
+        kdo: "Janik",
+        pozn1: "pozn1",
+        pozn2: "pozn2",
+        pozn3: "pozn3",
+      },
+      {
+        id: 2,
+        datum: "2.1.2021",
+        den: "ut",
+        kdo: "Janik",
+        pozn1: "pozn1",
+        pozn2: "pozn2",
+        pozn3: "pozn3",
+      },
+      {
+        id: 3,
+        datum: "3.1.2021",
+        den: "st",
+        kdo: "Janik",
+        pozn1: "pozn1",
+        pozn2: "pozn2",
+        pozn3: "pozn3",
+      },
+      {
+        id: 4,
+        datum: "4.1.2021",
+        den: "ct",
+        kdo: "Janik",
+        pozn1: "pozn1",
+        pozn2: "pozn2",
+        pozn3: "pozn3",
+      },
+      {
+        id: 5,
+        datum: "5.1.2021",
+        den: "pa",
+        kdo: "Dalibor",
+        pozn1: "pozn1",
+        pozn2: "pozn2",
+        pozn3: "pozn3",
+      },
+      {
+        id: 6,
+        datum: "6.1.2021",
+        den: "so",
+        kdo: "Dalibor",
+        pozn1: "pozn1",
+        pozn2: "pozn2",
+        pozn3: "pozn3",
+      },
+      {
+        id: 7,
+        datum: "7.1.2021",
+        den: "ne",
+        kdo: "Dalibor",
+        pozn1: "pozn1",
+        pozn2: "pozn2",
+        pozn3: "pozn3",
+      },
     ],
     chaty: [
-      { id: 1, datum: '9.5.2020 16:32:21', denId: 9, kdo: 'Janik', text: 'Tak to teda nevím' },
-      { id: 2, datum: '9.5.2020 17:32:16', denId: 9, kdo: 'Tomik', text: 'Zato ja vim' },
-      { id: 3, datum: '6.5.2020 09:12:21', denId: 2, kdo: 'Janik', text: 'Dneska taky nic nevim' },
-      { id: 4, datum: '10.5.2020 02:45:55', denId: 9, kdo: 'Janik', text: 'To nebude ono' },
-      { id: 5, datum: '11.5.2020 19:32:01', denId: 9, kdo: 'Tomik', text: 'Finálně!!!!' },
-      { id: 6, datum: '15.5.2020 09:00:21', denId: 3, kdo: 'Janik', text: 'Ddneska jsem zase dutej' }
+      {
+        id: 1,
+        datum: "9.5.2020 16:32:21",
+        denId: 9,
+        kdo: "Janik",
+        text: "Tak to teda nevím",
+      },
+      {
+        id: 2,
+        datum: "9.5.2020 17:32:16",
+        denId: 9,
+        kdo: "Tomik",
+        text: "Zato ja vim",
+      },
+      {
+        id: 3,
+        datum: "6.5.2020 09:12:21",
+        denId: 2,
+        kdo: "Janik",
+        text: "Dneska taky nic nevim",
+      },
+      {
+        id: 4,
+        datum: "10.5.2020 02:45:55",
+        denId: 9,
+        kdo: "Janik",
+        text: "To nebude ono",
+      },
+      {
+        id: 5,
+        datum: "11.5.2020 19:32:01",
+        denId: 9,
+        kdo: "Tomik",
+        text: "Finálně!!!!",
+      },
+      {
+        id: 6,
+        datum: "15.5.2020 09:00:21",
+        denId: 3,
+        kdo: "Janik",
+        text: "Ddneska jsem zase dutej",
+      },
     ],
     kdo: null,
-    idToken: null
-
+    idToken: null,
   },
   getters: {
     denByID: (state) => (id: number) => {
-      return state.dny.find(den => den.id === id)
+      return state.dny.find((den) => den.id === id);
     },
     // neni last chat
     lastChat: (state) => (denId: number) => {
-      const chatyDen = state.chaty.filter(chaty => chaty.denId === denId)
+      const chatyDen = state.chaty.filter((chaty) => chaty.denId === denId);
       if (chatyDen.length > 0) {
         const chatMaxId = chatyDen.reduce(function (prev, current) {
           if (+current.id > +prev.id) {
-            return current
+            return current;
           } else {
-            return prev
+            return prev;
           }
-        })
-        const chat = state.chaty.find(chaty => chaty.id === chatMaxId.id)
-        return chat.kdo + '  napsal ' + chat.text
+        });
+        const chat = state.chaty.find((chaty) => chaty.id === chatMaxId.id);
+        return chat.kdo + "  napsal " + chat.text;
       }
     },
     chatyDen: (state) => (denId: number) => {
-      return state.chaty.filter(chaty => denId === chaty.denId)
+      return state.chaty.filter((chaty) => denId === chaty.denId);
     },
     karty: (state) => (id: number) => {
-      const chatyDen = state.chaty.filter(chaty => chaty.denId === id)
+      const chatyDen = state.chaty.filter((chaty) => chaty.denId === id);
 
       const chatMaxId = chatyDen.reduce(function (prev, current) {
         if (+current.id > +prev.id) {
-          return current
+          return current;
         } else {
-          return prev
+          return prev;
         }
-      })
+      });
       return {
-        den: state.dny.find(den => den.id === id),
-        chat: state.chaty.find(chaty => chaty.id === chatMaxId.id)
-      }
+        den: state.dny.find((den) => den.id === id),
+        chat: state.chaty.find((chaty) => chaty.id === chatMaxId.id),
+      };
     },
     lastChatId: (state) => () => {
       const lastchat = state.chaty.reduce(function (prev, current) {
         if (+current.id > +prev.id) {
-          return current
+          return current;
         } else {
-          return prev
+          return prev;
         }
-      })
-      return lastchat.id
-    }
+      });
+      return lastchat.id;
+    },
   },
   mutations: {
-    INSERT_CHAT (state, chat) {
-      state.chaty.push(chat)
+    INSERT_CHAT(state, chat) {
+      state.chaty.push(chat);
       // $set(this.chaty, this.lastChatId - 1, chat)
     },
-    SAVE_EDIT (state, pDen) {
-      const denIndex = state.dny.findIndex(nden => nden.id === pDen.id)
-      state.dny[denIndex] = pDen
-      console.log(state.dny)
+    SAVE_EDIT(state, pDen) {
+      const denIndex = state.dny.findIndex((nden) => nden.id === pDen.id);
+      state.dny[denIndex] = pDen;
+      console.log(state.dny);
     },
-    SAVE_LOGIN (state, login) {
-      state.kdo = login.login
-      state.idToken = login.idToken
-      console.log(login)
+    SAVE_LOGIN(state, login) {
+      state.kdo = login.login;
+      state.idToken = login.idToken;
+      console.log(login);
     },
-    DELETE_TOKEN (state) {
-      state.idToken = ''
-    }
+    DELETE_TOKEN(state) {
+      state.idToken = "";
+    },
+    LOAD_DATA(state, res) {
+      state.dny = res;
+    },
   },
   actions: {
     insertChat: ({ commit }, payload) => {
-      commit('INSERT_CHAT', payload)
+      commit("INSERT_CHAT", payload);
     },
     saveEdit: ({ commit }, payload) => {
-      commit('SAVE_EDIT', payload)
+      commit("SAVE_EDIT", payload);
     },
-    saveLogin: ({ commit }, payload: {login: string;password: string}) => {
+    saveLogin: ({ commit }, payload: { login: string; password: string }) => {
       // console.log(payload)
-      const idToken = Math.random().toString(36).substr(2)
-      const celyLogin: {login: string; password: string; idToken?: string} = { login: payload.login, password: payload.password, idToken }
+      const idToken = Math.random().toString(36).substr(2);
+      const celyLogin: { login: string; password: string; idToken?: string } = {
+        login: payload.login,
+        password: payload.password,
+        idToken,
+      };
       // console.log(celyLogin)
-      celyLogin.idToken = idToken
+      celyLogin.idToken = idToken;
       // console.log(celyLogin)
-      commit('SAVE_LOGIN', celyLogin)
+      commit("SAVE_LOGIN", celyLogin);
     },
     removeToken: ({ commit }) => {
-      commit('DELETE_TOKEN')
+      commit("DELETE_TOKEN");
     },
-    initLoad: ({ commit }) => {
-      axios.get(process.env.BCK_URL + '/dny')
-      commit('LOAD_DATA', resp.data)
-    }
+    async initLoad({ commit }) {
+      // const res = await axios.get(process.env.BCK_URL + '/dny')
+      const res = await axios.get(
+        "https://arcane-dawn-99940.herokuapp.com/kal/dny"
+      );
+      commit("LOAD_DATA", res);
+    },
   },
-  modules: {
-
-  }
-})
+  modules: {},
+});
